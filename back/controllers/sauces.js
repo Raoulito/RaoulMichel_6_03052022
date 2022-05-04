dc937598af50afebbbc21df340228c68497fbe7e
Raoul <raoul.michel1@gmail.com>
2022-05-04 17:11:15 +0200

    V3 - likes and dislikes are working

--- back/controllers/sauces.js
+++ back/controllers/sauces.js
@@ -102,38 +102,38 @@ exports.getAllSauces = (req, res) => {
     .catch((error) => res.status(500).json({ error }));
 };
 
-// (dis)Likes a sauce
+//Définit le statut « Like » pour l' userId fourni. Si like = 1, l'utilisateur like la sauce. Si like = 0, l'utilisateur annule son like ou son dislike. Si like = -1,l'utilisateur n'aime pas (=
+// dislike) la sauce. L'ID de l'utilisateur doit être ajouté ou retiré du tableau approprié. Cela permet de garder une trace de leurs préférences et les empêche de liker ou de ne pas disliker
+//la même sauce plusieurs fois : un utilisateur ne peut avoir qu'une seule valeur pour chaque sauce. Le nombre total de « Like » et de « Dislike » est mis à jour à chaque nouvelle notation.
 exports.likeSauce = (req, res) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
       if (!sauce) {
         return res.status(404).json({ message: "Sauce non trouvée !" });
       }
-      //If the user has already liked the sauce
       if (sauce.usersLiked.includes(req.body.userId)) {
-        sauce.usersLiked = sauce.usersLiked.filter(
-          (userId) => userId !== req.body.userId
+        sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId), 1);
+        sauce.likes--;
+      }
+      if (sauce.usersDisliked.includes(req.body.userId)) {
+        sauce.usersDisliked.splice(
+          sauce.usersDisliked.indexOf(req.body.userId),
+          1
         );
-        sauce.likes = sauce.likes + 1;
-      } else {
+        sauce.dislikes--;
+      }
+      if (req.body.like === 1) {
         sauce.usersLiked.push(req.body.userId);
-        sauce.likes = sauce.likes - 1;
+        sauce.likes++;
+      }
+      if (req.body.like === -1) {
+        sauce.usersDisliked.push(req.body.userId);
+        sauce.dislikes++;
       }
       sauce
         .save()
         .then((sauce) => res.status(200).json(sauce))
         .catch((error) => res.status(500).json({ error }));
-        //If the user has already disliked the sauce
-      if (sauce.usersDisliked.includes(req.body.userId)) {
-        sauce.usersDisliked = sauce.usersDisliked.filter(
-          (userId) => userId !== req.body.userId
-        );
-        sauce.dislikes = sauce.dislikes - 1;
-      }
-      else {
-        sauce.usersDisliked.push(req.body.userId);
-        sauce.dislikes = sauce.dislikes + 1;
-      }
     })
     .catch((error) => res.status(500).json({ error }));
-}
+};
